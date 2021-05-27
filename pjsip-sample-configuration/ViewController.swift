68a212991f94cf2744224b11cf9b92b37c68d185
Muhammad Adeel <84326156+Adeel-NowSoftware@users.noreply.github.com>
2021-05-27 17:59:18 +0500

    Initial Commit

---
+++ pjsip-sample-configuration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pjsip-sample-configuration
+//
+//  Created by Now Software on 5/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
